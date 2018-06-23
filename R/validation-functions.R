07387c4a9ca4ca1d8668a4789aab9a1d23da0638
DillonHammill <dillonh21@live.com.au>
2018-06-23 10:18:25 +1000

    Update validation functions to include gate_type check

--- R/validation-functions.R
+++ R/validation-functions.R
@@ -1,7 +1,9 @@
-#' Check Supplied Channels
+#' Check Supplied Channels.
 #' 
 #' @param x an object of class \code{flowFrame}, \code{flowSet} or \code{GatingSet}.
 #' @param channels vector of length 1 or 2 indicating the names of the channels to use for plotting and gating.
+#'
+#' @return Stops gating process if channels are incorrect or returns nothing.
 checkChannels <- function(x, channels){
   
   chans <- colnames(x)
@@ -22,10 +24,31 @@ checkChannels <- function(x, channels){
       
     }
     
-  }else{
-    
-    return(channels)
+  }
   
+}
+
+#' Check Supplied Gate Types(s).
+#' 
+#' @param gate_type vector indicating the types of gates to construct using \code{drawGate}.
+#' 
+#' @return Stops gating process if gate_type is incorrect or returns nothing.
+checkGateType <- function(gate_type){
+  
+  gts <- c("polygon", "Polygon", "p", "P","rectangle", "Rectangle", "r", "R","interval", "Interval", "i", "I","threshold", "Threshold", "t", "T", "boundary", "Boundary", "b", "B","ellipse", "Ellipse", "e", "E","quadrant", "Quadrant", "q", "Q")
+  
+  if(!all(gate_type %in% gts)){
+    
+  if(length(gate_type[gate_type %in% gts == FALSE]) >= 2){
+      
+    stop(paste(paste(gate_type[gate_type %in% gts == FALSE], collapse = " & "), "are not valid gate_types for drawGate!"))
+      
+  }else{
+      
+    stop(paste(gate_type[gate_type %in% gts == FALSE],"is not a valid gate_type for drawGate!"))
+      
+  }
+    
   }
   
 }
