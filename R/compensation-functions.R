edc51618f0fa3f704f9d7f32f8fbffea02e25c08
DillonHammill <dillonh21@live.com.au>
2018-06-30 21:46:34 +1000

    Update computeSpillover function for flowSet input - need to add methods for flowSet and GatingSet.

--- R/compensation-functions.R
+++ R/compensation-functions.R
@@ -1,24 +1,33 @@
-#' Caluclate Spillover Matrix
+#' Caluclate Spillover Matrix - flowSet Method
 #' 
 #' @param fs object of class \code{flowSet} containing compensation single stain controls as well as an unstained control.
+#' @param gtfile openCyto \code{gatingTemplate} csv file to be used to pre-gate samples prior to spillover calculation. If no
+#' \code{gatingTemplate} is supplied samples will be gated FSC-A/SSC-A singlets gated in SSC-W/SSC-H using \code{drawGate}. The gating
+#' strategy will be saved in gatingTemplate csv file called \code{"Compensation gatingTemplate.csv"}.
+#' @param alias name of the gated population to use for downstream calculations, set to the last node of the GatingSet by default.
+#' @param pdfile \code{pData} csv file containing additional column \code{"channel"} indicating the fluorescent channel associated
+#' with each sample. This channel should be set to \code{"Unstained"} for unstained controls.
+#' @param spfile name of the output spillover csv file, set to \code{"Spillover Matrix.csv"} by default.
+#' 
+#' @importFrom flowWorkspace getData
 #' 
 #' @return spillover matrix and \code{"Spillover Matrix.csv"} file.
 #' 
 #' @export
-computeSpillover <- function(fs, file = NULL){
+computeSpillover <- function(fs, gtfile = NULL, alias = NULL, pdfile = NULL, spfile = NULL){
   
   # Extract fluorescent channels
   channels <- getChannels(fs)
   
   # Select a fluorescent channel for each compensation control
-  if(is.null(file)){
+  if(is.null(pdfile)){
  
   pData(fs)$channel <- selectChannels(fs)
-  write.csv(pData(fs), "Compensation Control Channels.csv", row.names = 1)
+  write.csv(pData(fs), "Compensation Control Channels.csv", row.names = FALSE)
   
   }else{
     
-    pData(fs) <- read.csv(file, header = TRUE)
+    pData(fs) <- read.csv(pdfile, header = TRUE, row.names = 1)
     
   }
   
@@ -26,27 +35,47 @@ computeSpillover <- function(fs, file = NULL){
   gs <- GatingSet(fs)
   
   # Transform fluorescent channels
-  trans <- estimateLogicle(gs[[1]], channels)
+  trans <- estimateLogicle(gs[[1]], c(channels,"SSC-A","SSC-H","SSC-W"))
   gs <- transform(gs, trans)
   
-  gs <- drawGate(gs, alias = "Cells", channels = c("FSC-A","SSC-A"), gate_type = "polygon", subSample = 250000,file = "Compensation gatingTemplate.csv")
-  gs <- drawGate(gs, alias = "Single Cells", channels = c("SSC-W","SSC-H"), gate_type = "polygon", subSample = 250000,file = "Compensation gatingTemplate.csv")
-  fs <- getData(gs, "Single Cells")
+  # Gate Samples
+  if(!is.null(gtfile)){
+    
+    gt <- gatingTemplate(gtfile)
+    gating(gt,gs)
+    
+  }else if(is.null(gtfile)){
+    
+    drawGate(gs, alias = "Cells", channels = c("FSC-A","SSC-A"), gate_type = "polygon", subSample = 250000,file = "Compensation gatingTemplate.csv")
+    drawGate(gs, alias = "Single Cells", channels = c("SSC-W","SSC-H"), gate_type = "polygon", subSample = 250000,file = "Compensation gatingTemplate.csv")
+
+  }
+  
+  # Extract Population for Downstream Analyses
+  if(!is.null(alias)){
+    
+    fs <- flowWorkspace::getData(gs, alias)
+    
+  }else if(is.null(alias)){
+  
+    fs <- flowWorkspace::getData(gs, getNodes(gs)[length(getNodes(gs))])
+    
+  }
   
   # Extract unstained control based on selected channels in pData(fs)
-  NIL <- fs[match("Unstained", pData(fs)$channel)]
+  NIL <- fs[[match("Unstained", pData(fs)$channel)]]
   fs <- fs[-match("Unstained", pData(fs)$channel)]
   
   # Assign channel to each flowFrame to description slot called "Channel"
   sapply(1:length(pData(fs)$channel), function(x){
-    fs[[x]]@description$Channel <- pData(fs)$channel[x]
+    fs[[x]]@description$Channel <- paste(pData(fs)$channel[x])
   })
   
   # Gate positive populations
   pops <- fsApply(fs, function(fr){
     
     # Call drawGate on each flowFrame using interval gate on selected channel
-    gt <- drawGate(fr, alias = paste(fr@description$Channel,"+"), channels = fr@description$Channel, gate_type = "interval")
+    gt <- drawGate(x = fr, alias = paste(fr@description$Channel,"+"), channels = fr@description$Channel, gate_type = "interval", adjust = 1.5)
     fr <- Subset(fr, gt[[1]])
     
   }, simplify = TRUE)
@@ -55,19 +84,15 @@ computeSpillover <- function(fs, file = NULL){
   inv <- transformList(names(trans), lapply(trans, `[[`, "inverse"))
   pops <- transform(pops, inv)
   NIL <- transform(NIL, inv)
-  NIL <- NIL[[1]]
-  
+
   # Calculate MedFI for all channels for unstained control
   neg <- each_col(NIL, median)[channels]
-  print(neg)
   
   # Calculate MedFI for all channels for all stained controls
   pos <- fsApply(pops, each_col, median)[,channels]
-  print(pos)
   
   # Subtract background fluorescence
   signal <- sweep(pos, 2, neg)
-  print(signal)
   
   # Construct spillover matrix - only include values for which there is a control
   spill <- diag(x = 1, nrow = length(channels), ncol = length(channels))  
@@ -75,12 +100,10 @@ computeSpillover <- function(fs, file = NULL){
   rownames(spill) <- channels
   
   # Normalise each row to stained channel
-  sapply(1:nrow(signal), function(x){
+  for(i in 1:nrow(signal)){
     
-    signal[x, ] <- signal[x, ]/signal[x, match(fs[[x]]@description$Channel, colnames(spill))]
-    
-  })
-  print(signal)
+    signal[i, ] <- signal[i, ]/signal[i, match(fs[[i]]@description$Channel, colnames(spill))]
+  } 
   
   # Insert values into appropriate rows
   rws <- match(pData(fs)$channel, rownames(spill))
