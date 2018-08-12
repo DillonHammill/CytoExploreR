2e04ec7c887a5893cd1b13dbfbb617d1dbc793a7
Dillon Hammill <Dillon.Hammill@anu.edu.au>
2018-08-12 10:54:06 +1000

    Add openCyto deparsing function to convert existing gate to csv entry.

---
+++ R/openCyto-parsing-functions.R
@@ -0,0 +1,24 @@
+#' deparse a list(named) of expression into a string 
+#' inverse function of argParser
+#' 
+#' @param args gatingTemplate arguments as named list to be deparsed (e.g. list(gate = gateobj)).
+#' 
+#' @return deparsed arguments for storage in gatingTemplate csv file
+#' 
+#' @export
+.argDeparser <- function(args, split = TRUE){
+  if(split){
+    args <- sapply(names(args), function(argn){
+      
+      argv <- deparse(args[[argn]])
+      argv <- gsub("\"", "'", argv) #restore dquote to squote
+      argv <- paste(argv, collapse = "")
+      paste(argn, argv, sep = " = ")
+    })
+    
+    
+    paste(args, collapse = ", ")  
+  }else
+    as.character(args[[1]])
+  
+}
