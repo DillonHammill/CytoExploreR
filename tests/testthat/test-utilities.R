944cc6025866b6ab8ef146fea5072c77ed1b5467
Dillon Hammill <Dillon.Hammill@anu.edu.au>
2019-05-06 11:33:01 +1000

    Add unit tests for utilities.

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -5,6 +5,45 @@ context("utilities")
 test_that(".empty", {
   
   expect_true(.empty(""))
+  expect_false(.empty("test"))
   expect_true(.empty(c("","")))
+  
+})
+
+# .all_na ----------------------------------------------------------------------
+
+test_that(".all_na", {
+  
+  expect_true(.all_na(NA))
+  expect_false(.all_na(""))
+  expect_false(.all_na(c(NA,"test")))
+  expect_true(.all_na(list(NA,NA,NA)))
+  
+})
+
+# args_list --------------------------------------------------------------------
+
+test_that("args_list", {
+  
+  f <- function(x,
+                y = 1,
+                z = 3){
+    
+    args_list()
+    
+  }
+  ref <- alist("x" = "", "y" = 1, "z" = 3)
+  expect_equal(f()[["x"]], "")
+  expect_equal(f()[["y"]], 1)
+  expect_equal(f()[["z"]], 3)
+  
+})
+
+# file_wd_check ----------------------------------------------------------------
+
+test_that("file_wd_check", {
+  
+  expect_true(file_wd_check("Activation-gatingTemplate.csv"))
+  expect_false(file_wd_check("Test.csv"))
   
 })
