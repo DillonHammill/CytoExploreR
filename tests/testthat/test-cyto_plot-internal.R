f129e16d3954a94009d64b8d65974070be11ee86
Dillon Hammill <Dillon.Hammill@anu.edu.au>
2019-10-10 12:11:51 +1100

    Remove old unit tests.

--- tests/testthat/test-cyto_plot-internal.R
+++
@@ -1,8 +0,0 @@
-context("cyto_plot-internal")
-
-# .cyto_plot_1d ----------------------------------------------------------------
-
-CytoRSuite:::.cyto_plot_1d.flowFrame(Va2[[1]],
-                          channels = "CD4",
-                          axes_trans = trans,
-                          border_fill = "black")
--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -2,11 +2,20 @@ context("zzz")
 
 # zzz ---------------------------------------------------------------
 
-test_that("CytoRSuite loading", {
-  CytoRSuite:::.onLoad()
-  expect_true(getOption("CytoRSuite_interact") == interactive())
-  expect_true(getOption("CytoRSuite_wd_check") == TRUE)
-  expect_true(openCyto:::.isRegistered("gate_manual"))
-  expect_true(openCyto:::.isRegistered("pp_gate_draw"))
-  expect_true(openCyto:::.isRegistered("gate_draw"))
+test_that("CytoExploreR loading", {
+  CytoExploreR:::.onLoad()
+  expect_false(getOption("locatorBell"))
+  expect_equal(getOption("CytoExploreR_gatingTemplate"), NULL)
+  expect_true(getOption("CytoExploreR_wd_check") == TRUE)
+  expect_equal(getOption("cyto_plot_call"), NULL)
+  expect_equal(getOption("cyto_plot_match"), NULL)
+  expect_equal(getOption("cyto_plot_theme"), NULL)
+  expect_false(getOption("cyto_plot_save"))
+  expect_equal(getOption("cyto_plot_method"), NULL)
+  expect_false(getOption("cyto_plot_custom"))
+  expect_false(getOption("cyto_plot_grid"))
+  expect_equal(getOption("cyto_plot_label_coords"), NULL)  
+  expect_true(openCyto:::.isRegistered("cyto_gate_manual"))
+  expect_true(openCyto:::.isRegistered("pp_cyto_gate_draw"))
+  expect_true(openCyto:::.isRegistered("cyto_gate_draw"))
 })
