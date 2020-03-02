a2ccf1158332b738f77de6cc4818c91e48b9906f
Dillon Hammill <Dillon.Hammill@anu.edu.au>
2020-03-02 11:38:30 +1100

    Update cyto_map internals to include separate mapping function. Transformations only affects saved files now. Add new merge argument to allow individual mapping of samples.

--- R/cyto_map.R
+++ R/cyto_map.R
@@ -2,6 +2,13 @@
 
 #' Create dimension-reduced maps of cytometry data
 #'
+#' \code{cyto_map} is a convenient wrapper to produce dimension-reduced maps of
+#' cytometry data using PCA, tSNE, FIt-SNE, UMAP and EmbedSOM. These
+#' dimensionality reduction functions are called using the default settings, but
+#' can be altered by passing relvant arguments through \code{cyto_map}. To see a
+#' full list of customisable parameters refer to the documentation for each of
+#' these functions by clicking on the links below.
+#'
 #' @param x object of class \code{flowFrame} or \code{flowSet}.
 #' @param parent name of the parent population to extract from
 #'   \code{GatingHierarchy} or \code{GatingSet} objects for mapping, set to the
@@ -18,6 +25,8 @@
 #'   are mapped by default.
 #' @param type dimension reduction type to use to generate the map, supported
 #'   options include "PCA", "tSNE", "FIt-SNE", "UMAP" and "EmbedSOM".
+#' @param merge logical indicating whether the samples should be merged prior to
+#'   mapping to generate a consensus map, set to TRUE by default.
 #' @param split logical indicating whether samples merged using
 #'   \code{cyto_merge_by} should be split prior to writing fcs files, set to
 #'   FALSE by default.
@@ -61,8 +70,12 @@
 #' @seealso \code{\link[EmbedSOM:SOM]{SOM}}
 #' @seealso \code{\link[EmbedSOM:EmbedSOM]{EmbedSOM}}
 #'
+#' @references  Gabriel K. (1971). The biplot graphical display of matrices with
+#'   application to principal component analysis. Biometrika 58, 453–467.
+#'   \url{doi:10.1093/biomet/58.3.453}.
 #' @references Maaten, L. van der, & Hinton, G. (2008). Visualizing Data using
 #'   t-SNE. Journal of Machine Learning Research 9, 2579–2605.
+#'   \url{http://www.jmlr.org/papers/volume9/vandermaaten08a/vandermaaten08a.pdf}.
 #' @references Linderman, G., Rachh, M., Hoskins, J., Steinerberger, S.,
 #'   Kluger., Y. (2019). Fast interpolation-based t-SNE for improved
 #'   visualization of single-cell RNA-seq data. Nature Methods.
@@ -73,7 +86,7 @@
 #'   Fišer, K., Sedlacek, R., Vondrášek, J., and Drbal, K. (2018). Rapid
 #'   single-cell cytometry data visualization with EmbedSOM. Preprint at
 #'   \url{https://www.biorxiv.org/content/10.1101/496869v1}.
-
+#'   
 #' @author Dillon Hammill, \email{Dillon.Hammill@anu.edu.au}
 #'
 #' @name cyto_map
@@ -93,6 +106,7 @@ cyto_map.GatingSet <- function(x,
                                channels = NULL,
                                display = 1,
                                type = "UMAP",
+                               merge = TRUE,
                                split = TRUE,
                                names = NULL,
                                save_as = NULL,
@@ -111,7 +125,8 @@ cyto_map.GatingSet <- function(x,
   gs_clone <- cyto_copy(x)
   
   # EXTRACT DATA
-  fs <- cyto_extract(gs_clone, parent = parent)
+  fs <- cyto_extract(gs_clone, 
+                     parent = parent)
   
   # NAMES
   if(is.null(names)){
@@ -119,27 +134,44 @@ cyto_map.GatingSet <- function(x,
   }  
   
   # MERGE
-  fr <- cyto_merge_by(fs, merge_by = "all")[[1]]
+  if(length(fs) > 1 & merge == TRUE){
+    fr_list <- cyto_merge_by(fs, 
+                             merge_by = "all")
+  }else{
+    split <- FALSE
+    fr_list <- lapply(cyto_names(fs), function(z){
+      fs[[z]]
+    })
+    names(fr_list) <- cyto_names(fs)
+  }
   
-  # FLOWFRAME METHOD - FLOWFRAME/FLOWSET RETURN
-  map_data <- cyto_map(fr,
-                       channels = channels,
-                       display = display,
-                       type = type,
-                       split = split,
-                       names = names,
-                       save_as = save_as,
-                       inverse = inverse,
-                       trans = trans,
-                       plot = FALSE,
-                       seed = seed, ...)
+  # FLOWFRAME METHOD - FLOWFRAME/FLOWSET RETURN (DEPENDS ON MERGE/SPLIT)
+  map_data <- lapply(seq_along(fr_list), function(z){
+    cyto_map(fr,
+             channels = channels,
+             display = display,
+             type = type,
+             split = split,
+             names = names,
+             save_as = save_as,
+             inverse = inverse,
+             trans = trans,
+             plot = FALSE,
+             seed = seed, ...)
+  })
+  
+  # FLOWFRAME LIST TO FLOWSET
+  if(length(map_data) > 1){
+    map_data <- flowSet_to_cytoset(flowSet(map_data))
+  }else{
+    map_data <- map_data[[1]]
+  }
   
   # SPLIT MERGED FLOWFRAME
   if(is(map_data, "flowFrame")){
     map_data <- cyto_split(map_data,
                            names = names)
-    map_data <- flowSet(map_data)
-    map_data <- flowSet_to_cytoset(map_data)
+    map_data <- flowSet_to_cytoset(flowSet(map_data))
   }
   
   # UPDATE GATINGSET - CLONE
@@ -185,6 +217,7 @@ cyto_map.flowSet <- function(x,
                              channels = NULL,
                              display = 1,
                              type = "UMAP",
+                             merge = TRUE,
                              split = TRUE,
                              names = NULL,
                              save_as = NULL,
@@ -202,29 +235,46 @@ cyto_map.flowSet <- function(x,
     x <- cyto_select(x, select)
   }
   
-  # MERGE SAMPLES
-  fr <- cyto_merge_by(x, 
-                      merge_by = "all")[[1]]
+  # MERGE MULTIPLE SAMPLES
+  if(merge == TRUE){
+    fr_list <- cyto_merge_by(x, merge_by = "all")
+  }else{
+    # CAN'T SPLIT UNMERGED SAMPLES
+    split <- FALSE
+    fr_list <- lapply(cyto_names(x), function(z){
+      x[[z]]
+    })
+    names(fr_list) <- cyto_names(x)
+  }
   
   # NAMES
   if(is.null(names)){
     names <- cyto_names(x)
   }
   
-  # FLOWFRAME METHOD
-  x <- cyto_map(fr,
-                channels = channels,
-                display = display,
-                type = type,
-                split = split,
-                names = names,
-                save_as = save_as,
-                inverse = inverse,
-                trans = trans,
-                plot = plot,
-                seed = seed, ...)
+  # FLOWFRAME METHOD - EACH LIST ELEMENT
+  x <- lapply(fr_list, function(z){
+    cyto_map(z,
+             channels = channels,
+             display = display,
+             type = type,
+             split = split,
+             names = names,
+             save_as = save_as,
+             inverse = inverse,
+             trans = trans,
+             plot = plot,
+             seed = seed, ...)
+  })
   
-  # RETURN MAPPED DATA - FLOWFRAME/FLOWSET
+  # FLOWFRAME LIST TO FLOWSET
+  if(length(x) > 1){
+    x <- flowSet_to_cytoset(flowSet(x))
+  }else{
+    x <- x[[1]]
+  }
+  
+  # RETURN MAPPED DATA - FLOWFRAME/FLOWSET (DEPENDS ON MERGE & SPLIT)
   return(x)
   
 }
@@ -256,6 +306,7 @@ cyto_map.flowFrame <- function(x,
                                           "Event ID",
                                           "PCA",
                                           "tSNE",
+                                          "FIt-SNE",
                                           "UMAP",
                                           "EmbedSOM"))
     channels <- channels[channels %in% names(cyto_markers(x))]
@@ -281,66 +332,11 @@ cyto_map.flowFrame <- function(x,
   
   # MAPPING --------------------------------------------------------------------
   
-  # MESSAGE
-  message(paste0("Computing ", type, " co-ordinates..."))
-  
-  # SET SEED - RETURN SAME MAP WITH EACH RUN
-  if(!is.null(seed)){
-    set.seed(seed)
-  }
-  
-  # PCA
-  if(grepl(type, "PCA", ignore.case = TRUE)){
-    # MAPPING
-    mp <- prcomp(fr_exprs, ...)
-    # MAPPING CO-ORDINATES
-    coords <- mp$x[, 1:2, drop = FALSE]
-    colnames(coords) <- c("PCA-1","PCA-2")
-  # tSNE
-  }else if(grepl(type, "tSNE", ignore.case = TRUE)){
-    # MAPPING 
-    mp <- Rtsne(fr_exprs, ...)
-    # MAPPING CO-ORDINATES
-    coords <- mp$Y
-    colnames(coords) <- c("tSNE-1","tSNE-2")
-  # FIt-SNE
-  }else if(grepl(type, "FIt-SNE", ignore.case = TRUE) |
-           grepl(type, "FItSNE", ignore.case = TRUE, fixed = TRUE)){  
-    mp <- fftRtsne(fr_exprs, ...)
-    # MAPPING CO-ORDINATES
-    coords <- mp
-    colnames(coords) <- c("FIt-SNE-1", "FIt-SNE-2")  
-  # UMAP 
-  }else if(grepl(type, "UMAP", ignore.case = TRUE)){
-    # MAPPING
-    mp <- umap(fr_exprs, ...)
-    # MAPPING CO-ORDINATES
-    coords <- mp$layout
-    colnames(coords) <- c("UMAP-1","UMAP-2")
-  # EmbedSOM
-  } else if(grepl(type, "EmbedSOM", ignore.case = TRUE)){
-    # DATA
-    data <- fr_exprs
-    # PULL DOWN ARGUMENTS
-    args <- .args_list(...)
-    # CREATE SOM - FLOWSOM NOT SUPPLIED (fsom)
-    if(!"fsom" %in% names(args)){
-      # SOM
-      mp <- do.call("SOM", 
-                    args[names(args) %in% formalArgs(EmbedSOM::SOM)])
-      # SOM ARGUMENTS
-      args[["map"]] <- mp
-    }
-    # EMBEDSOM
-    mp <- do.call("EmbedSOM",
-                  args[names(args) %in% formalArgs(EmbedSOM::EmbedSOM)])
-    # MAPPING CO-ORDINATES
-    coords <- mp
-    colnames(coords) <- c("EmbedSOM-1", "EmbedSOM-2")
-  # UNSUPPORTED TYPE  
-  }else{
-    stop(paste(type, "is not a supported mapping type."))
-  }
+  # MAPPPING COORDS
+  coords <- .cyto_map(fr_exprs,
+                      type = type,
+                      seed = seed, 
+                      ...)
   
   # ADD MAPPING COORDS TO FLOWFRAME
   x <- fr_append_cols(x, coords)
@@ -357,24 +353,103 @@ cyto_map.flowFrame <- function(x,
              })
   }
   
-  # SAVE MAPPED FLOWFRAME(S) ---------------------------------------------------
+  # SPLIT MAPPED FLOWFRAME -----------------------------------------------------
   
-  # CYTO_SAVE
-  if(!is.null(save_as)){
-    x <- cyto_save(x,
-                   split = split,
-                   names = names,
-                   save_as = save_as,
-                   inverse = inverse,
-                   trans = trans)
+  # SPLIT -> FLOWSET (ELSE MERGED FLOWFRAME)
+  if(split == TRUE & "Sample ID" %in% cyto_channels(x)){
+    x <- cyto_split(x, names = names)
+    x <- flowSet_to_cytoset(flowSet(x))
   }
   
-  # SPLIT FLOWFRAMES TO FLOWSET
-  if(is(x, "list")){
-    x <- flowSet_to_cytoset(flowSet(x))
+  # SAVE MAPPED SAMPLES --------------------------------------------------------
+  
+  # CYTO_SAVE - INVERSE TRANSFORMS ONLY APPLIED FOR SAVING
+  if(!is.null(save_as)){
+    cyto_save(x,
+              split = FALSE,
+              names = names,
+              save_as = save_as,
+              inverse = inverse,
+              trans = trans)
   }
   
   # RETURN MAPPED FLOWFRAME/FLOWSET --------------------------------------------
   return(x)
+  
+}
+
+## INTERNAL MAPPING FUNCTION ---------------------------------------------------
+
+#' Obtain dimension-reduced co-ordinates
+#' @param x matrix containing the data to be mapped.
+#' @noRd 
+.cyto_map <- function(x,
+                      type = "UMAP",
+                      seed = NULL, 
+                      ...){
+  
+  # MESSAGE
+  message(paste0("Computing ", type, " co-ordinates..."))
+  
+  # SET SEED - RETURN SAME MAP WITH EACH RUN
+  if(!is.null(seed)){
+    set.seed(seed)
+  }
+  
+  # PCA
+  if(grepl(type, "PCA", ignore.case = TRUE)){
+    # MAPPING
+    mp <- prcomp(x, ...)
+    # MAPPING CO-ORDINATES
+    coords <- mp$x[, 1:2, drop = FALSE]
+    colnames(coords) <- c("PCA-1","PCA-2")
+    # tSNE
+  }else if(grepl(type, "tSNE", ignore.case = TRUE)){
+    # MAPPING 
+    mp <- Rtsne(x, ...)
+    # MAPPING CO-ORDINATES
+    coords <- mp$Y
+    colnames(coords) <- c("tSNE-1","tSNE-2")
+    # FIt-SNE
+  }else if(grepl(type, "FIt-SNE", ignore.case = TRUE) |
+           grepl(type, "FItSNE", ignore.case = TRUE, fixed = TRUE)){  
+    mp <- fftRtsne(x, ...)
+    # MAPPING CO-ORDINATES
+    coords <- mp
+    colnames(coords) <- c("FIt-SNE-1", "FIt-SNE-2")  
+    # UMAP 
+  }else if(grepl(type, "UMAP", ignore.case = TRUE)){
+    # MAPPING
+    mp <- umap(x, ...)
+    # MAPPING CO-ORDINATES
+    coords <- mp$layout
+    colnames(coords) <- c("UMAP-1","UMAP-2")
+    # EmbedSOM
+  } else if(grepl(type, "EmbedSOM", ignore.case = TRUE)){
+    # DATA
+    data <- x
+    # PULL DOWN ARGUMENTS
+    args <- .args_list(...)
+    # CREATE SOM - FLOWSOM NOT SUPPLIED (fsom)
+    if(!"fsom" %in% names(args)){
+      # SOM
+      mp <- do.call("SOM", 
+                    args[names(args) %in% formalArgs(EmbedSOM::SOM)])
+      # SOM ARGUMENTS
+      args[["map"]] <- mp
+    }
+    # EMBEDSOM
+    mp <- do.call("EmbedSOM",
+                  args[names(args) %in% formalArgs(EmbedSOM::EmbedSOM)])
+    # MAPPING CO-ORDINATES
+    coords <- mp
+    colnames(coords) <- c("EmbedSOM-1", "EmbedSOM-2")
+    # UNSUPPORTED TYPE  
+  }else{
+    stop(paste(type, "is not a supported mapping type."))
+  }
+  
+  # RETURN MAPPED COORDS
+  return(coords)
   
 }
