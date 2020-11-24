238865545531e2b9563709d3f5057f51c4db0b3d
Dillon Hammill <Dillon.Hammill@anu.edu.au>
2020-11-24 14:10:21 +1100

    Refine cyto_plot_contour into a single function.

--- R/cyto_plot_contour.R
+++ R/cyto_plot_contour.R
@@ -3,10 +3,14 @@
 #' Add contour lines to cyto_plot
 #'
 #' @param x either an object of class
-#'   \code{\link[flowCore:flowFrame-class]{flowFrame}}, or
-#'   \code{\link[flowCore:flowSet-class]{flowSet}} or a list of
-#'   \code{\link[flowCore:flowFrame-class]{flowFrame}} objects.
+#'   \code{\link[flowWorkspace:cytoframe]{cytoframe}},
+#'   \code{\link[flowWorkspace:cytoset]{cytoset}} or a list of
+#'   \code{\link[flowWorkspace:cytoframe]{cytoframe}} objects.
+#' @param overlay optional argument if x is a cytoframe to overlay a list of
+#'   cytoframe objects.
 #' @param channels channels used to construct the existing cyto_plot.
+#' @param display controls the number or percentage of events to display, set to
+#'   1 by default to display all events.
 #' @param contour_lines numeric indicating the number of levels to use for
 #'   contour lines, set to 15 by default.
 #' @param contour_line_type type of line to use for contour lines, set to 1 by
@@ -20,9 +24,7 @@
 #'
 #' @importFrom MASS kde2d
 #' @importFrom graphics contour par
-#' @importFrom flowCore exprs
 #' @importFrom grDevices adjustcolor
-#' @importFrom methods is
 #'
 #' @examples
 #' library(CytoExploreRData)
@@ -48,136 +50,51 @@
 #'   channels = c("Alexa Fluor 488-A", "Alexa Fluor 700-A"),
 #'   contour_lines = 20
 #' )
-#'
 #' @author Dillon Hammill, \email{Dillon.Hammill@anu.edu.au}
 #'
-#' @name cyto_plot_contour
-NULL
-
-#' @rdname cyto_plot_contour
 #' @export
-cyto_plot_contour <- function(x, ...){
-  UseMethod("cyto_plot_contour")
-}
-
-#' @rdname cyto_plot_contour
-#' @export
-cyto_plot_contour.flowFrame <- function(x,
-                                        channels,
-                                        contour_lines = 15,
-                                        contour_line_type = 1,
-                                        contour_line_width = 1,
-                                        contour_line_col = "black",
-                                        contour_line_alpha = 1,
-                                        ...){
+cyto_plot_contour <- function(x,
+                              channels,
+                              overlay = NA,
+                              display = 1,
+                              contour_lines = 15,
+                              contour_line_type = 1,
+                              contour_line_width = 1,
+                              contour_line_col = "black",
+                              contour_line_alpha = 1,
+                              ...) {
   
   # CHECKS -------------------------------------------------------------------
   
-  # CHANNELS
-  channels <- cyto_channels_extract(x, channels)
-  
-  # PREPARE DATA -------------------------------------------------------------
-  fr <- x
-  fr_exprs <- exprs(fr)[, channels]
-  
-  # CONTOUR_LINES ------------------------------------------------------------
-  if (contour_lines != 0) {
-    # BYPASS INSUFFICIENT EVENTS
-    if (nrow(fr_exprs) > 2) {
-      # 2D KERNEL DENSITY
-      z <- kde2d(
-        x = fr_exprs[, 1],
-        y = fr_exprs[, 2],
-        n = 75,
-        lims = par("usr")
-      )
-      # PLOT CONTOUR LINES
-      graphics::contour(
-        z = z$z,
-        x = z$x,
-        y = z$y,
-        add = TRUE,
-        drawlabels = FALSE,
-        nlevels = contour_lines,
-        col = adjustcolor(contour_line_col, contour_line_alpha),
-        lwd = contour_line_width,
-        lty = contour_line_type
-      )
+  # X - FLOWFRAME/FLOWFRAME LIST/FLOWSET/CYTO_PLOT ARGS
+  if(cyto_class(x, "flowFrame")) {
+    overlay <- cyto_list(overlay)
+    if(!.all_na(overlay)) {
+      x <- c(structure(list(x), names = cyto_names(x)),
+             overlay)
+    } else {
+      x <- cyto_list(x)
     }
+  } else if(cyto_class(x, "flowSet")) {
+    x <- cyto_list(x)
+  } else if(cyto_class(x, "cyto_plot")) {
+    .args_update(x)
+  } else if(!all(LAPPLY(x, cyto_class, "flowFrame"))) {
+    stop("'x' must be a list of cytoframe objects!")
   }
-  
-}
-
-#' @rdname cyto_plot_contour
-#' @export
-cyto_plot_contour.flowSet <- function(x,
-                                      channels,
-                                      contour_lines = 15,
-                                      contour_line_type = 1,
-                                      contour_line_width = 1,
-                                      contour_line_col = "black",
-                                      contour_line_alpha = 1,
-                                      ...){
-  
-  # CHECKS ---------------------------------------------------------------------
   
   # CHANNELS
-  channels <- cyto_channels_extract(x, channels)
-  
-  # PREPARE DATA ---------------------------------------------------------------
-  
-  # LIST OF FLOWFRAMES
-  fr_list <- lapply(seq_len(length(x)), function(z) {
-    x[[z]]
-  })
-  
-  # CONTOUR_LINES --------------------------------------------------------------
-  invisible(mapply(
-    function(fr,
-             contour_lines,
-             contour_line_type,
-             contour_line_width,
-             contour_line_col,
-             contour_line_alpha) {
-      cyto_plot_contour(fr,
-                        channels = channels,
-                        contour_lines = contour_lines,
-                        contour_line_type = contour_line_type,
-                        contour_line_width = contour_line_width,
-                        contour_line_col = contour_line_col,
-                        contour_line_alpha = contour_line_alpha
-      )
-    }, fr_list,
-    contour_lines,
-    contour_line_type,
-    contour_line_width,
-    contour_line_col,
-    contour_line_alpha
-  ))
-}
-
-#' @rdname cyto_plot_contour
-#' @export
-cyto_plot_contour.list <- function(x,
-                                   channels,
-                                   contour_lines = 15,
-                                   contour_line_type = 1,
-                                   contour_line_width = 1,
-                                   contour_line_col = "black",
-                                   contour_line_alpha = 1,
-                                   ...){
-  
-  # CHECKS ---------------------------------------------------------------------
-  
-  # CLASS
-  if (!all(lapply(x, is, "flowFrame"))) {
-    stop("x should be a list of flowFrame objects.")
-  }
-  
-  # CHANNELS 
   channels <- cyto_channels_extract(x[[1]], channels)
   
-  # CONTOUR_LINES --------------------------------------------------------------
+  # SAMPLING
+  if(display != 1) {
+    x <- cyto_sample(x,
+                     display = display,
+                     seed = 56)
+  }
+  
+  # CONTOUR_LINES ------------------------------------------------------------
+  
   invisible(mapply(
     function(fr,
              contour_lines,
@@ -185,15 +102,35 @@ cyto_plot_contour.list <- function(x,
              contour_line_width,
              contour_line_col,
              contour_line_alpha) {
-      cyto_plot_contour(fr,
-                        channels = channels,
-                        contour_lines = contour_lines,
-                        contour_line_type = contour_line_type,
-                        contour_line_width = contour_line_width,
-                        contour_line_col = contour_line_col,
-                        contour_line_alpha = contour_line_alpha
-      )
-    }, x,
+      fr_exprs <- cyto_data_extract(fr, 
+                                    raw = TRUE, 
+                                    channels = channels)[[1]]
+      if (contour_lines != 0) {
+        # BYPASS INSUFFICIENT EVENTS
+        if (nrow(fr_exprs) > 2) {
+          # 2D KERNEL DENSITY
+          z <- kde2d(
+            x = fr_exprs[, 1],
+            y = fr_exprs[, 2],
+            n = 75,
+            lims = par("usr")
+          )
+          # PLOT CONTOUR LINES
+          graphics::contour(
+            z = z$z,
+            x = z$x,
+            y = z$y,
+            add = TRUE,
+            drawlabels = FALSE,
+            nlevels = contour_lines,
+            col = adjustcolor(contour_line_col, contour_line_alpha),
+            lwd = contour_line_width,
+            lty = contour_line_type
+          )
+        }
+      }
+    }, 
+    x,
     contour_lines,
     contour_line_type,
     contour_line_width,
