55c4ad4bc2c134fc2da3a6d2c76e051d79dfa019
Dillon Hammill <Dillon.Hammill@anu.edu.au>
2021-04-15 13:36:08 +1000

    Update cyto_plot_contour to accept all data types. Add seed argument to ensure consistent sampling.

--- R/cyto_plot_contour.R
+++ R/cyto_plot_contour.R
@@ -4,10 +4,13 @@
 #'
 #' @param x either an object of class
 #'   \code{\link[flowWorkspace:cytoframe]{cytoframe}},
-#'   \code{\link[flowWorkspace:cytoset]{cytoset}} or a list of
-#'   \code{\link[flowWorkspace:cytoframe]{cytoframe}} objects.
-#' @param overlay optional argument if x is a cytoframe to overlay a list of
-#'   cytoframe objects.
+#'   \code{\link[flowWorkspace:cytoset]{cytoset}},
+#'   \code{\link[flowWorkspace:GatingHierarchy-class]{GatingHierarchy}},
+#'   \code{\link[flowWorkspace:GatingSet-class]{GatingSet}} or a list containing
+#'   cytoframe or cytoset objects.
+#' @param parent name of the parent population to extract from GatingHierarchy
+#'   or GatingSt objects, set to the "root" node by default.
+#' @param overlay list containing cytoframe or cytoset objects to overlay.
 #' @param channels channels used to construct the existing cyto_plot.
 #' @param display controls the number or percentage of events to display, set to
 #'   1 by default to display all events.
@@ -20,6 +23,10 @@
 #'   default.
 #' @param contour_line_alpha numeric [0,1] to control transparency of contour
 #'   lines, set to 1 by default to remove transparency.
+#' @param seed numeric passed to \code{\link{set.seed}} to ensure that the same
+#'   sampling is applied with each \code{\link{cyto_plot_contour}} call, set to
+#'   an arbitrary numeric by default. This behaviour can be turned off by
+#'   setting this argument to NULL.
 #' @param ... not in use.
 #'
 #' @importFrom MASS kde2d
@@ -29,31 +36,30 @@
 #' @examples
 #' library(CytoExploreRData)
 #'
-#' # Load in Samples
-#' fs <- Activation
-#'
-#' # Apply compensation
-#' fs <- cyto_compensate(fs)
-#'
-#' # Transform fluorescent channels
-#' trans <- cyto_transformer_logicle(fs)
-#' fs <- cyto_transform(fs, trans)
-#'
-#' # Plot
-#' cyto_plot(fs[[32]],
-#'   channels = c("Alexa Fluor 488-A", "Alexa Fluor 700-A"),
-#'   axes_trans = trans
+#' # Activation GatingSet
+#' gs <- cyto_load(
+#'   system.file(
+#'     "extdata/Activation-GatingSet",
+#'     package = "CytoExploreRData"
+#'   )
 #' )
 #'
-#' # Contour lines
-#' cyto_plot_contour(fs[[32]],
-#'   channels = c("Alexa Fluor 488-A", "Alexa Fluor 700-A"),
-#'   contour_lines = 20
-#' )
+#' # Visualise T Cells
+#' cyto_plot(gs[[1]],
+#' parent = "T Cells",
+#' channels = c("CD4", "CD8"))
+#'
+#' # Add contour lines
+#' cyto_plot_contour(gs[[1]],
+#' parent = "T Cells",
+#' channels = c("CD4", "CD8"),
+#' contour_lines = 15)
+#'
 #' @author Dillon Hammill, \email{Dillon.Hammill@anu.edu.au}
 #'
 #' @export
 cyto_plot_contour <- function(x,
+                              parent = "root",
                               channels,
                               overlay = NA,
                               display = 1,
@@ -62,56 +68,73 @@ cyto_plot_contour <- function(x,
                               contour_line_width = 1,
                               contour_line_col = "black",
                               contour_line_alpha = 1,
+                              seed = 42,
                               ...) {
   
   # CHECKS -------------------------------------------------------------------
   
-  # X - FLOWFRAME/FLOWFRAME LIST/FLOWSET/CYTO_PLOT ARGS
-  if(cyto_class(x, "flowFrame")) {
-    overlay <- cyto_list(overlay)
-    if(!.all_na(overlay)) {
-      x <- c(structure(list(x), names = cyto_names(x)),
-             overlay)
-    } else {
-      x <- cyto_list(x)
-    }
-  } else if(cyto_class(x, "flowSet")) {
-    x <- cyto_list(x)
-  } else if(cyto_class(x, "cyto_plot")) {
-    .args_update(x)
-  } else if(!all(LAPPLY(x, cyto_class, "flowFrame"))) {
-    stop("'x' must be a list of cytoframe objects!")
-  }
   
   # CHANNELS
-  channels <- cyto_channels_extract(x[[1]], channels)
+  channels <- cyto_channels_extract(x, channels)
+  
+  # EXTRACT DATA
+  if(cyto_class(x, "GatingSet")) {
+    x <- cyto_data_extract(gs,
+                           parent = parent,
+                           channels = channels,
+                           copy = FALSE)
+  }
+  
+  # PREPARE DATA
+  if(cyto_class(x,c("flowFrame", "flowSet"))) {
+    # COMBINE LAYERS
+    if(!.all_na(overlay)) {
+      if(cyto_class(overlay, c("flowFrame", "flowSet"))) {
+        overlay <- list(overlay)
+      }
+      x <- c(list(x), overlay)
+    } else {
+      x <- list(x)
+    }
+  # CYTO_PLOT ARGUMENTS
+  } else if(cyto_class(x, "cyto_plot")) {
+    .args_update(x)
+  }
   
   # SAMPLING
   if(display != 1) {
     x <- cyto_sample(x,
                      display = display,
-                     seed = 56)
+                     seed = seed)
   }
   
   # CONTOUR_LINES ------------------------------------------------------------
   
   invisible(mapply(
-    function(fr,
+    function(cf,
              contour_lines,
              contour_line_type,
              contour_line_width,
              contour_line_col,
              contour_line_alpha) {
-      fr_exprs <- cyto_data_extract(fr, 
+      # EXTRACT RAW DATA
+      cf_exprs <- cyto_data_extract(cf, 
                                     format = "matrix", 
                                     channels = channels)[[1]]
+      # MERGE CYTOSET MULTIPLE SAMPLES
+      if(length(cf_exprs) > 1) {
+        cf_exprs <- do.call("rbind", cf_exprs)
+      } else {
+        cf_exprs <- cf_exprs[[1]]
+      }
+      # ADD CONTOUR LINES
       if (contour_lines != 0) {
         # BYPASS INSUFFICIENT EVENTS
-        if (nrow(fr_exprs) > 2) {
+        if (nrow(cf_exprs) > 2) {
           # 2D KERNEL DENSITY
           z <- kde2d(
-            x = fr_exprs[, 1],
-            y = fr_exprs[, 2],
+            x = cf_exprs[, 1],
+            y = cf_exprs[, 2],
             n = 75,
             lims = par("usr")
           )
