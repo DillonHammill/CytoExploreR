23c0df27afed79affcb74e7bdba7c9df905b00fc
Dillon Hammill <Dillon.Hammill@anu.edu.au>
2021-11-12 13:07:30 +1100

    Improve handling of stored unmixing details in cyto_unmix_compute().

---
+++ R/cyto_file_search.R
@@ -0,0 +1,101 @@
+## CYTO_FILE_SEARCH ------------------------------------------------------------
+
+#' Search for CSV file in current directory
+#'
+#' @param x regular expression to use when searching for files by name in
+#'   specified directory.
+#' @param dir location where the search should be performed.
+#' @param rownames minimal requirement for row names in the file.
+#' @param colnames minimal requirement for column names in the file.
+#' @param ... additional named arguments indicating values to find in particular
+#'   columns of the file (e.g. name = c("Activation_1.fcs")).
+#'
+#' @return NULL or data read in from file matching search criteria.
+#'
+#' @author Dillon Hammill, \email{Dillon.Hammill@anu.edu.au}
+#'
+#' @examples 
+#' \dontrun{ 
+#'   # search for experiment details of samples
+#'   cyto_file_search( 
+#'     "Experiment-Details.csv$", 
+#'     rownames = c("Activation_1.fcs", "Activation_2.fcs")
+#'   ) 
+#' }
+#'
+#' @export
+cyto_file_search <- function(x,
+                             dir = ".",
+                             colnames = NULL,
+                             rownames = NULL,
+                             ...) {
+  
+  # FILES IN DIRECTORY
+  files <- list.files(
+    dir,
+    pattern = x,
+    recursive = TRUE,
+    include.dirs = FALSE,
+    full.names = TRUE
+  )
+  
+  # FILES FOUND
+  if(length(files) > 0) {
+    # COLUMNS TO SEARCH
+    cols <- list(...)
+    
+    # SEARCH FILES
+    files <- structure(
+      lapply(
+        files,
+        function(z) {
+          # READ FILE
+          f <- read_from_csv(
+            z
+          )
+          # CHECK FILE COLUMN NAMES
+          if(!is.null(colnames)) {
+            if(!all(colnames %in% colnames(f))) {
+              return(NULL)
+            }
+          }
+          # CHECK FILE ROW NAMES
+          if(!is.null(rownames)) {
+            if(!all(rownames %in% rownames(f))) {
+              return(NULL)
+            }
+          }
+          # CHECK COLUMNS
+          if(length(cols) > 0) {
+            stop <- FALSE
+            lapply(
+              names(cols),
+              function(w) {
+                if(!all(cols[[w]] %in% f[, cols[[w]]])) {
+                  stop <<- TRUE
+                }
+              }
+            )
+            if(stop) {
+              return(NULL)
+            }
+          }
+          return(f)
+        }
+      ),
+      names = files
+    )
+    
+    # RETURN DATA
+    files <- files[!LAPPLY(files, "is.null")]
+  }
+  
+  # NO FILES FOUND
+  if(length(files) == 0) {
+    files <- NULL
+  }
+  
+  # READ IN DATA IN LIST
+  return(files)
+  
+}
--- R/cyto_spectra_unmix.R
+++ R/cyto_spectra_unmix.R
@@ -1,9 +1,501 @@
-cyto_spectra_unmix <- function(x, 
+## CYTO_UNMIX_COMPUTE ----------------------------------------------------------
+
+#' Compute spectral unmixing matrix
+#'
+#' \code{cyto_unmix_compute()} will guide the user through the steps to required
+#' to compute the spectral unmixing matrix using single colour control samples.
+#'
+#' \code{cyto_unmix_compute()} does not perform any gating internally so samples
+#' should be gated beforehand to isolate homogeneous populations of cells and/or
+#' beads. \code{cyto_unmix_compute()} also requires annotation of some
+#' additional information in \code{cyto_details(x)} prior to computing the
+#' unmixing matrix. This includes \itemize{\item{group}{indicates the type of
+#' each sample (i.e. cells or beads)}\item{parent}{name of the population to
+#' extract from each sample (e.g. single cells or single beads) when a
+#' GatingHierarchy or Gatingset is supplied}\item{label}{a name for the new
+#' unmixed parameter for each control (e.g. CD4 FITC) - labels for unstained
+#' controls should be left empty}}. Each \code{group} must contain a reference
+#' unstained population in order to compute the unmixing matrix.
+#'
+#' @param x object of class \code{\link[flowWorkspace:cytoset]{cytoset}} or
+#'   \code{\link[flowWorkspace:GatingSet-class]{GatingSet}} containing single
+#'   colour controls.
+#' @param channels names of the channels/markers for which unmixing coefficients
+#'   should be computed, set to all fluorescent channels by default.
+#' @param select passed to \code{cyto_select()} to extract a subset of samples
+#'   for computation of unmixing matrix.
+#' @param auto indicates which group (see \code{group_by}) should be used to get
+#'   the levels of background autofluorescence.
+#' @param save_as name of a CSV to which the computed unmixing matrix should be
+#'   written, set to \code{Spectral-Unmixing-Matrix.csv} prefixed with the date
+#'   by default. Set this argument to NA if you don't want to write the unmixing
+#'   matrix to file.
+#'
+#' @return computed unmixing coefficients in the form of a matrix.
+#'
+#' @author Dillon Hammill, \email{Dillon.Hammill@anu.edu.au}
+#'
+#' @examples
+#' \dontrun{
+#' # GatingSet of controls
+#' unmix <- cyto_unmix_compute(
+#'   gs,
+#'   group_by = "type",
+#'   auto = "cells",
+#'   save_as = "Spectral-Unmixing-Matrix.csv"
+#' )
+#' }
+#'
+#' @export
+cyto_unmix_compute <- function(x,
+                               channels = NULL,
+                               select = NULL,
+                               auto = NULL,
+                               save_as = NULL,
                                ...) {
+  
+  # TODO: EDITOR SHOULD ALWAYS OPEN IN INTERACTIVE MODE
+  
+  # PREPARE DATA ---------------------------------------------------------------
+  
+  # SELECT
+  x <- cyto_select(x, select)
+  
+  # CHANNELS
+  if(is.null(channels)) {
+    channels <- cyto_fluor_channels(x)
+  }
+  
+  # UNMIXING DETAILS
+  pd <- cyto_details(x)
+  
+  # REQUIRED VARIABLES
+  vars <- c(
+    "name",
+    "group",
+    "parent",
+    "label"
+  )
+  
+  # REQUIRED COLUMNS
+  vars_req <- c()
+  lapply(
+    vars,
+    function(z) {
+      if(any(grepl(z, colnames(pd), ignore.case = TRUE))) {
+        colnames(pd)[grep(z, colnames(pd), ignore.case = TRUE)[1]] <<- z
+      } else {
+        vars_req <<- c(vars_req, z)
+      }
+    }
+  )
+  
+  # REQUIRED COLUMNS MISSING
+  if(length(vars_req) > 0) {
+    # SEARCH FOR UNMIXING DETAILS
+    pd_new <- cyto_file_search(
+      "-Unmixing-Details.csv$",
+      colnames = c("name", "group", "parent", "label"),
+      rownames = rownames(cyto_details(x)),
+    )
+    # NO UNMIXING DETAILS FOUND
+    if(length(pd_new) == 0) {
+      # UNMIXING DETAILS FILE
+      file <- NULL
+      # APPEND MISSING VARIABLES
+      pd <- cbind(
+        pd, 
+        matrix(
+          NA,
+          nrow = nrow(pd),
+          ncol = length(vars_req),
+          dimnames = list(
+            rownames(pd),
+            vars_req
+          )
+        )
+      )
+    # UNMIXING DETAILS FOUND
+    } else {
+      # UNMIXING DETAILS FILE
+      file <- names(pd_new)[1]
+      # APPEND MISSING VARIABLES
+      pd <- cbind(
+        pd, 
+        pd_new[[1]][, vars_req]
+      )
+    }
+  }
+  
+  # UPDATE EXPERIMENT DETAILS
+  cyto_details(x) <- pd
+  
+  # INTERACTIVELY EDIT UNMIXING DETAILS
+  if(interactive() & cyto_option("CytoExploreR_interactive")) {
+    x <- cyto_details_edit(
+      x,
+      file = NA,
+      col_options = if(cyto_class(x, "GatingSet")) {
+        list("parent" = cyto_nodes(x))
+      } else {
+        NULL
+      }
+    )
+  }
+  
+  # UPDATE UNMIXING DETAILS
+  pd <- cyto_details(x)
+  
+  # MISSING VARIABLES
+  if(!all(vars %in% colnames(pd))) {
+    stop(
+      paste0(
+        "cyto_details(x) is missing required variables: ",
+        paste0(
+          vars[!vars %in% colnames(pd)],
+          collapse = " & "
+        )
+      )
+    )
+  }
+  
+  # FILE - SAVE UNMIXING DETAILS
+  if(is.null(file)) {
+    file <- paste0(
+      format(
+        Sys.Date(), 
+        "%d%m%y"
+      ), 
+      "-Spectral-Unmixing-Details.csv"
+    )
+  }
+  
+  # EXPORT UPDATED UNMIXING DETAILS
+  write_to_csv(
+    pd,
+    file,
+    row.names = TRUE
+  )
+  
+  # SPLIT DATA INTO GROUPS
+  cs_list <- cyto_group_by(
+    x,
+    group_by = "group"
+  )
+  
+  # AUTOFLUORESCENCE
+  if(is.null(auto)) {
+    # SINGLE GROUP
+    if(length(cs_list) == 1) {
+      auto <- names(cs_list)
+    # MULTIPLE GROUPS - SELECTION REQUIRED
+    } else {
+      # ENQUIRE
+      if(interactive() & cyto_option("CytoExploreR_interactive")) {
+        message(
+          paste0(
+            "Which of the following groups do you want to use for the ",
+            "autofluorescence parameter? \n"
+          )
+        )
+        message(
+          paste0(
+            1:length(cs_list), 
+            ": ", 
+            names(cs_list),
+            sep = "\n"
+          )
+        )
+        auto <- cyto_enquire(NULL)
+        if(!is.na(suppressWarnings(as.numeric(auto)))) {
+          auto <- names(cs_list)[auto]
+        }
+        # AUTO REQUIRED
+      } else {
+        stop(
+          paste0(
+            "Supply the name of the group to use for autofluorescence ",
+            "parameter to 'auto'."
+          )
+        )
+      }
+    }
+  }
+  
+  # EXTRACT DATA - EACH GROUP SAME PARENT
+  cs_list <- structure(
+    lapply(
+      seq_along(cs_list),
+      function(z) {
+        cyto_data_extract(
+          cs_list[[z]],
+          parent = cyto_details(cs_list[[z]])$parent[1],
+          channels = channels
+        )[[1]]
+      }
+    ),
+    names = names(cs_list)
+  )
+  
+  # COMPUTE SPECTRAL UNMIXING MATRIX -------------------------------------------
+  
+  # COMPUTE MEDFI - ALL CHANNELS
+  medFI_list <- structure(
+    lapply(
+      cs_list,
+      function(z) {
+        do.call(
+          "rbind",
+          cyto_apply(
+            z,
+            FUN = "cyto_stat_median",
+            input = "matrix",
+            simplify = FALSE
+          )
+        )
+      }
+    ),
+    names = names(cs_list)
+  )
+  
+  # SWEEP OUT UNSTAINED SIGNAL PER GROUP
+  unmix <- do.call(
+    "rbind",
+    structure(
+      lapply(
+        seq_along(medFI_list),
+        function(z) {
+          # UNSTAINED INDEX
+          ind <- which.min(rowSums(medFI_list[[z]]))
+          # SWEEP OUT UNSTAINED SIGNAL
+          m <- sweep(
+            medFI_list[[z]],
+            2,
+            medFI_list[[z]][ind, , drop = FALSE],
+            "-"
+          )[-ind, , drop = FALSE]
+          # AUTOFLUORESCENCE
+          if(auto %in% names(medFI_list)[z]) {
+            m <- rbind(
+              m, 
+              "*auto*" = medFI_list[[z]][ind, ]
+            )
+          }
+          return(m)
+        }
+      ),
+      names = names(medFI_list)
+    )
+  )
+  
+  # PROPORTION OF SIGNAL IN EACH DETECTOR
+  unmix <- t(
+    apply(
+      unmix,
+      1,
+      function(z){
+        z / max(z)
+      }
+    )
+  )
+  
+  # REMOVE NEGATIVE VALUES
+  unmix[unmix < 0] <- 0
+  
+  # LABELS
+  rownames(unmix) <- LAPPLY(
+    rownames(unmix),
+    function(z) {
+      ind <- tryCatch(
+        cyto_match(
+          x,
+          z,
+          exact = TRUE
+        ),
+        error = function(e) {
+          return(NULL)
+        }
+      )
+      if(is.null(ind)) {
+        return(
+          "Autofluorescence"
+        )
+      } else {
+        return(
+          pd$label[ind]
+        )
+      }
+    }
+  )
+  
+  # DEFAULT FILENAME
+  if(is.null(save_as)) {
+    save_as <- paste0(
+      format(
+        Sys.Date(), 
+        "%d%m%y"
+      ), 
+      "-Spectral-Unmixing-Matrix.csv"
+    )
+  }
+  
+  # SAVE MATRIX
+  if(!.all_na(save_as)) {
+    write_to_csv(
+      unmix, 
+      save_as,
+      row.names = TRUE
+    )
+  }
+
+  # SPECTRAL UNMIXING MATRIX
+  return(unmix)
   
 }
 
-cyto_unmix <- function(x, 
-                       ...){
+## CYTO_UNMIX ------------------------------------------------------------------
+
+#' Apply spectral unmixing matrix to spectral data
+#'
+#' @param x an object of class \code{\link[flowWorkspace:cytoframe]{cytoframe}},
+#'   \code{\link[flowWorkspace:cytoset]{cytoset}},
+#'   \code{\link[flowWorkspace:GatingHierarchy-class]{GatingHierarchy}} or
+#'   \code{\link[flowWorkspace:GatingSet-class]{GatingSet}} containing unmixed
+#'   spectral data.
+#' @param unmix an unmixing matrix or the name of a CSV file containing the
+#'   unmixing matrix.
+#' @param copy logical indicating whether unmixing should be applied to a copy
+#'   of the original data, set to FALSE by default.
+#' @param ... not in use.
+#'
+#' @return unmixed \code{cytoframe}, \code{cytoset}, \code{GatingHierarchy} or
+#'   \code{GatingSet}.
+#'
+#' @author Dillon Hammill, \email{Dillon.Hammill@anu.edu.au}
+#'
+#' @importFrom stats lsfit
+#'
+#' @examples
+#' \dontrun{
+#' # Compute unmixing matrix
+#' unmix <- cyto_unmix_compute(
+#'   gs,
+#'   group_by = "type",
+#'   auto = "cells",
+#'   save_as = "Spectral-Unmixing-Matrix.csv"
+#' )
+#' # Apply unmixing matrix
+#' gs <- cyto_unmix(
+#'   gs,
+#'   unmix = unmix
+#' )
+#' }
+#'
+#' @name cyto_unmix
+NULL
+
+#' @noRd
+#' @export
+cyto_unmix <- function(x,
+                       ...) {
+  UseMethod("cyto_unmix")
+}
+
+#' @export
+cyto_unmix.default <- function(x,
+                               unmix = NULL,
+                               copy = FALSE,
+                               ...) {
   
+  # APPLY SPECTRAL UNMIXING
+  cs <- cyto_apply(
+    x,
+    parent = "root",
+    FUN = "cyto_unmix",
+    input = "cytoframe",
+    unmix = unmix,
+    copy = copy
+  )
+  
+  # UPDATE DATA IN GATINGHIERARCHY/GATINGSET
+  if(cyto_class(x, "GatingSet")) {
+    gs_cyto_data(x) <- cs
+    return(x)
+  # UNMIXED CYTOSET
+  } else {
+    return(cs)
+  }
+  
+}
+
+#' @export
+cyto_unmix.flowFrame <- function(x, 
+                                 unmix = NULL,
+                                 copy = FALSE,
+                                 ...){
+  
+  # MISSING UNMIX
+  if(is.null(unmix)) {
+    stop(
+      "Supply an unmixing matrix to 'unmix'."
+    )
+  # PREPARE UNMIX
+  } else {
+    # UNMIX FILE
+    if(is.character(unmix)) {
+      unmix <- read_from_csv(
+        unmix
+      )
+    }
+  }
+  
+  # CHANNELS
+  channels <- cyto_channels(x)
+  
+  # CHANNELS UNMIX
+  exprs <- cyto_exprs(
+    x,
+    channels = colnames(unmix),
+    drop = FALSE
+  )
+  
+  # LEAST SQUARES FIT
+  ls <- lsfit(
+    x = t(unmix),
+    y = t(exprs),
+    intercept = FALSE
+  )
+  
+  # UNMIX
+  unmix_coef <- t(ls$coefficients)
+  
+  # REMOVE UNMIXED PARAMETERS (EXTRA PARAMETERS REQUIRED OR CBIND NOT WORK)
+  rm <- cyto_channels(x) %in% colnames(unmix)
+  
+  # ALL CHANNELS (CAUSES ISSUES WITH CYTO_CBIND())
+  if(length(rm) == length(channels)) {
+    # KEEP A CHANNEL FOR CBINDING
+    x <- x[, rm[1]]
+    # ADD UNMIXED PARAMETERS
+    x <- cyto_cbind(
+      x,
+      unmix
+    )
+    # COPY & REMOVE EXCESS PARAMETER
+    return(
+      cyto_copy(x[, -match(rm[1], cyto_channels(x))])
+    )
+  # SOME EXTRA CHANNELS
+  } else {
+    # REMOVE RAW PARAMETERS
+    x <- x[, !cyto_channels(x) %in% colnames(unmix)]
+    # ADD UNMIXED PARAMETERS
+    x <- cyto_cbind(
+      x,
+      unmix_coef
+    )
+    # RETURN UNMIXED CYTOFRAME
+    return(x)
+  }
+
 }
